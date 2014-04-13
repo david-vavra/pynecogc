5db1669eef153bd3f6a3afbe59c19e671120110c
David Vavra <vavra@ics.muni.cz>
2014-04-13 16:54:57 +0200

    Change of the dir structure, setup.py added.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+__author__ = 'David Vavra'
+
+'''
+@author: David Vavra
+
+This script performs the installation of Pyrage. Core modules, altogether with example
+mako templates, xml network definition and fundamental modules.
+'''
+
+from distutils.core import setup
+
+setup(name='Pyrage',
+      version='0.1',
+      description='Python network config assessor and generator.'
+      'that gathers information about physical inventory of networking hardware.',
+      author='David Vavra',
+      author_email='vavra.david@email.cz',
+      platforms=['GNU/Linux'],
+      license='GNU',
+      packages=['pyrage',
+                'pyrage.modules',
+                ],
+      data_files=[('/usr/local/etc/pyrage/mako/', ['pyrage/mako/cisco_genconf.mako',
+                                                   'pyrage/mako/cisco_ncat.mako']),
+                  """
+                  ('/usr/local/etc/pyrage/xml/', ['pyrage/xml/devices.xsd',
+                                                  'pyrage/xml/devices-example.xml']),
+                  """
+                  ('/usr/local/bin/',['ncat_xmlconfig',
+                                      'pyge'])
+                  ]
+     )
