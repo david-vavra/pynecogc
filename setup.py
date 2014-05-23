4c40185ac93f9b5fe276cbaa1cf7c1bb8a2f5cb8
David Vavra <vavra@ics.muni.cz>
2014-05-23 21:08:45 +0200

    License.

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__author__ = 'David Vavra'
+"""
+    Copyright (C) 2014  David Vavra  (vavra.david@email.cz)
 
-'''
-@author: David Vavra
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-This script performs the installation of Pyrage. Core modules, altogether with example
-mako templates, xml network definition and fundamental modules.
-'''
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
 
 from setuptools import setup
 
